8d8ca3ccfe42b0e5ca13ea385730e3435cb10941
Rinseon <168741696+Rinseon@users.noreply.github.com>
2025-01-05 01:12:01 -0500

    Delete Bunni EXE UWU Installer.bat

--- Bunni EXE UWU Installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\rinse\Downloads\Bunni Cloud"  
-pyinstaller --clean --onefile --noconsole --name "Bunni Cloud" --icon="C:\Users\rinse\Downloads\Bunni Cloud\rabbit_head_icon.ico" "C:\Users\rinse\Downloads\Bunni Cloud\main.py"  
-pause
